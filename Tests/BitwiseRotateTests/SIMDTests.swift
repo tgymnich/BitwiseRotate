d67ab9deee748349b3bf9efa6e373197162d136e
Tim Gymnich <tgymnich@icloud.com>
2021-12-02 03:40:14 +0100

    rely only compiler specialisation

--- Tests/BitwiseRotateTests/SIMDTests.swift
+++ Tests/BitwiseRotateTests/SIMDTests.swift
@@ -4,14 +4,20 @@ import XCTest
 
 final class SIMDTests: XCTestCase {
     func testRotateLeft() {
-      let rotated = SIMD3<UInt8>(arrayLiteral: 0b01010100,0b11011100,0b00011000) <<< 1
+      let rotated1 = SIMD3<UInt8>(arrayLiteral: 0b01010100,0b11011100,0b00011000) <<< 1
+      var rotated2 = SIMD3<UInt8>(arrayLiteral: 0b01010100,0b11011100,0b00011000)
+      rotated2.rotatedLeft(1)
       let expected = SIMD3<UInt8>(arrayLiteral: 0b10101000, 0b10111001,0b00110000)
-      XCTAssertEqual(rotated, expected)
+      XCTAssertEqual(rotated1, expected)
+      XCTAssertEqual(rotated2, expected)
     }
   
   func testRotateRight() {
-    let rotated = SIMD3<UInt8>(arrayLiteral: 0b01010100,0b11011100,0b00011000) >>> 1
+    let rotated1 = SIMD3<UInt8>(arrayLiteral: 0b01010100,0b11011100,0b00011000) >>> 1
+    var rotated2 = SIMD3<UInt8>(arrayLiteral: 0b01010100,0b11011100,0b00011000)
+    rotated2.rotatedRight(1)
     let expected = SIMD3<UInt8>(arrayLiteral: 0b00101010, 0b01101110,0b00001100)
-    XCTAssertEqual(rotated, expected)
+    XCTAssertEqual(rotated1, expected)
+    XCTAssertEqual(rotated2, expected)
   }
 }
